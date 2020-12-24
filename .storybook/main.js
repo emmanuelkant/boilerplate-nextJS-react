4c58fa7a115d574c8a8aa357feb7200e8b42db5a
Emmanuel Kant Duarte <you@example.com>
2020-12-23 21:00:28 -0300

    updating storybook to accepting typescript by default

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-knobs/register', '@storybook/preset-typescript', '@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-knobs/register', '@storybook/addon-actions', '@storybook/addon-links'],
 };
